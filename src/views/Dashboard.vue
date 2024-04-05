5f2943079ae42ea7934b7e7825dcbe042e10c216
Desa Presisi <ddp@apps.ipb.ac.id>
2024-04-05 11:18:42 +0700

    layout video

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -208,52 +208,39 @@ import Footer from "../components/Footer.vue";
       >
         <h1 class="display-5 mb-4">Simak Informasi TBC</h1>
       </div>
-      <div class="row justify-content-center">
-        <div class="col-md col-lg wow fadeInUp py-2" data-wow-delay="0.3s">
-          <div class="container align-content-center mb-5">
-            <div class="row">
-              <div class="col-lg">
-                <div class="row gy-5 align-items-center">
-                  <div class="col-lg-6 wow fadeInLeft" data-wow-delay="0.2s">
-                    <iframe
-                      width="560"
-                      height="315"
-                      src="https://www.youtube.com/embed/CDV3pXcc_qg?si=_NUcUEcyCAxSNkSY"
-                      title="YouTube video player"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                      referrerpolicy="strict-origin-when-cross-origin"
-                      allowfullscreen
-                    ></iframe>
-                  </div>
-                  <div class="col-lg-6 wow fadeInRight" data-wow-delay="0.3s">
-                    <h4 class="mb-1 text-primary">Tentang Kami</h4>
-                    <h1 class="display-5 mb-4"></h1>
-                    <p class="mb-4">
-                      Gagasan TBC adalah ruang publik untuk bertemu, mendapatkan
-                      informasi akurat, serta berbagi gagasan untuk pencegahan
-                      dan pengobatan TBC yang efektif. Dalam portal Gagasan TBC
-                      ini terdapat 4 menu utama: (1) Dashboard; (2) Aksi TBC;
-                      (3) Ruang Gagasan; dan (4) Kontak. Kita dapat menyimak
-                      berita terkini, data statistik, serta konten edukasi dalam
-                      dashboard Gagasan. Seiring dengan perkembangan teknologi
-                      dan tantangan yang dihadapi, Pemerintah terus berupaya
-                      maksimal dalam kegiatan preventif & kuratif TBC. Upaya
-                      tersebut tercantum dalam Strategi Rencana Aksi pada menu
-                      Aksi TBC. Penanganan TBC Nasional dapat berjalan optimal
-                      dengan dukungan kolektif bersama, sehingga anda dapat
-                      berperan aktif dalam penanganan TBC Nasional dengan cara
-                      menyampaikan gagasan-gagasan anda dalam menu Ruang Gagasan
-                      pada portal ini. Mari kita dukung Indonesia Sehat menuju
-                      Indonesia Emas 2045!
-                    </p>
-                    <!-- <a href="#" class="btn btn-primary rounded-pill py-3 px-5"
-                      >Selengkapnya...</a
-                    > -->
-                  </div>
-                </div>
-              </div>
-            </div>
+
+      <div class="container align-content-center mb-5">
+        <div class="row gy-5 align-items-center">
+          <div class="col-lg-6 wow fadeInLeft" data-wow-delay="0.2s">
+            <iframe
+              class="w-100"
+              height="350"
+              src="https://www.youtube.com/embed/CDV3pXcc_qg?si=_NUcUEcyCAxSNkSY"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div class="col-lg-6 wow fadeInRight" data-wow-delay="0.3s">
+            <h4 class="mb-1 text-primary">Tentang Kami</h4>
+            <p class="mb-4">
+              Gagasan TBC adalah ruang publik untuk bertemu, mendapatkan
+              informasi akurat, serta berbagi gagasan untuk pencegahan dan
+              pengobatan TBC yang efektif. Dalam portal Gagasan TBC ini terdapat
+              4 menu utama: (1) Dashboard; (2) Aksi TBC; (3) Ruang Gagasan; dan
+              (4) Kontak. Kita dapat menyimak berita terkini, data statistik,
+              serta konten edukasi dalam dashboard Gagasan. Seiring dengan
+              perkembangan teknologi dan tantangan yang dihadapi, Pemerintah
+              terus berupaya maksimal dalam kegiatan preventif & kuratif TBC.
+              Upaya tersebut tercantum dalam Strategi Rencana Aksi pada menu
+              Aksi TBC. Penanganan TBC Nasional dapat berjalan optimal dengan
+              dukungan kolektif bersama, sehingga anda dapat berperan aktif
+              dalam penanganan TBC Nasional dengan cara menyampaikan
+              gagasan-gagasan anda dalam menu Ruang Gagasan pada portal ini.
+              Mari kita dukung Indonesia Sehat menuju Indonesia Emas 2045!
+            </p>
           </div>
         </div>
       </div>
@@ -261,36 +248,6 @@ import Footer from "../components/Footer.vue";
   </div>
   <!-- </div> -->
   <!-- video End -->
-
-  <!-- About Start -->
-  <!-- <div
-    class="container-fluid overflow-hidden px-5 py-2"
-    style="margin-top: 2rem"
-  >
-    <div class="container py-5">
-      <div class="row g-5">
-        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-          <div class="RotateMoveLeft">
-            <img src="/public/hero-about.svg" class="img-fluid w-70" alt="" />
-          </div>
-        </div>
-        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
-          <h4 class="mb-1 text-primary">Tentang Kami</h4>
-          <h1 class="display-5 mb-4"></h1>
-          <p class="mb-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, quae voluptate non sapiente labore tempora accusamus
-            architecto, aut, quibusdam rem reiciendis nemo voluptas iste culpa a
-            totam alias in harum!
-          </p>
-          <a href="#" class="btn btn-primary rounded-pill py-3 px-5"
-            >Selengkapnya...</a
-          >
-        </div>
-      </div>
-    </div>
-  </div> -->
-  <!-- About End -->
 
   <Footer></Footer>
 </template>
